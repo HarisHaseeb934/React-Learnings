c2d0ae1e9c713efc833ec0a3a71f4efb79d1733e
Haris <harishaseeb934@gmail.com>
2026-08-24 19:49:44 +0500

    Header CSS Complete

--- ApexStore/src/Components/Pages/Layout/Header.jsx
+++ ApexStore/src/Components/Pages/Layout/Header.jsx
@@ -6,24 +6,37 @@ import { IoPersonOutline } from "react-icons/io5";
 import { useState } from "react";
 
 const Header = () => {
-    const [isVisible, setVisible] = useState(false)
+  const [isVisible, setVisible] = useState(false);
+
+  function handleSerachVisibility() {
+    setVisible((prev) => !prev);
+  }
+
   return (
     <header className="w-full p-3">
-      <div className="w-full max-w-7xl m-auto flex justify-between items-center gap-4">
+      <div className="w-full max-w-7xl m-auto flex justify-between items-center relative">
         <div className="flex items-center justify-center">
           <img src="Logo.png" alt="" width="50px" />
-          <h1 className="text-2xl font-bold">ApexStore</h1>
+          <h1 className="sm:text-lg md:text-2xl font-bold">ApexStore</h1>
         </div>
-        <form className={`bg-slate-200 hidden md:flex items-center p-2 rounded-3xl md:w-xs`}>
-          <IoIosSearch className="text-xl" />
+        <form
+          className={`bg-slate-200 ${isVisible ? "nav hide" : "nav"} p-2 rounded-3xl md:w-xs`}
+        >
           <input
             type="text"
             name="search"
             className="outline-none px-2 text-md w-full"
             placeholder="Search Products..."
           />
+          <button>
+            <IoIosSearch className="text-xl" />
+          </button>
         </form>
-        <div className="flex items-center text-2xl gap-5">
+
+        <div className="flex items-center text-2xl gap-3 md:gap-5">
+          <button onClick={handleSerachVisibility}>
+            <IoIosSearch className="text-xl md:hidden" />
+          </button>
           <NavLink>
             <CiHeart />
           </NavLink>
